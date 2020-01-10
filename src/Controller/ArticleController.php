e28146e755dc1121f3ac45bd39ca1ce5cb2d89e6
valerie-geist <geistval@gmail.com>
2020-01-10 15:30:52 +0100

    ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,91 +1,71 @@
 <?php
-
-
 namespace App\Controller;
-
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Article;
 use App\Entity\Country;
 use App\Entity\Comment;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
 use App\Form\CommentFormType;
 use App\Repository\ArticleRepository;
 use App\Repository\CountryRepository;
+use App\Repository\CommentRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 use Doctrine\Common\Persistence\ObjectManager;
-
 class ArticleController extends AbstractController
 {
     private $articleRepository;
     private $countryRepository;
     private $commentRepository;
+    private $userRepository;
 
-
-    public function __construct(ArticleRepository $articleRepository, CountryRepository $countryRepository)
+    public function __construct(ArticleRepository $articleRepository, CountryRepository $countryRepository, CommentRepository $commentRepository, UserRepository $userRepository)
     {
         $this->articleRepository = $articleRepository;
         $this->countryRepository = $countryRepository;
         $this->commentRepository = $countryRepository;
+        $this->userRepository = $userRepository;
     }
-
-
 
     public function articlesAction(Request $request, $slug){
         $from = $request->query->get("from");
-        
-
         //recuperer le country a patir du slug
         $country = $this->countryRepository->findOneBy(["slug" => $slug]);
         $articles = $this->articleRepository->findBy(["country" => $country]);
-
         if(is_null($from) || $from < 1) {
             $from = 1;
         }
-
-
-
-
         return $this->render('user/pages/country.html.twig', [
-        "articles" => $articles,
-        "slug" => $slug,
+            "articles" => $articles,
+            "slug" => $slug,
         ]);
     }
 
-     public function CommentForm(Request $request, ObjectManager $manager){
-
-                 $comment = new Comment();
-                 $formComment = $this->createForm(CommentFormType::class, $comment);
-
-
-     }
-    public function articleAction($slug){
-
+    public function articleAction($slug, Request $request, Security $security){
         $article = $this->articleRepository->findOneBy(["slug" => $slug]);
 
-                 $comment = new Comment();
-                 $formComment = $this->createForm(CommentFormType::class, $comment);
+        $comment = new Comment();
+        $formComment = $this->createForm(CommentFormType::class, $comment);
+        $formComment->handleRequest($request);
 
-        return $this->render('user/pages/article.html.twig', [
-        "article" => $article,  "formComment" => $formComment->createView()
+        if($formComment->isSubmitted() && $formComment->isValid()){
 
-        ]);
-}
-
-
-/*  public function articlesAction(Request $request, $pages){
-         $from = $request->query->get("from");
-
-      if(is_null($page) || $page < 1) {
-            $page = 1;
+            $comment = $formComment->getData();
+            $user = $this->userRepository->findOneBy(['username' => $security->getUser()->getUsername()]);
+            $comment->setUser($user);
+            $comment->getId();
+            /* $article = $this->articleRepository->findOneBy(['id' = $id]); */
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute('article');
         }
+        return $this->render('user/pages/article.html.twig', [
+            "article" => $article, "comment" => $comment,  "formComment" => $formComment->createView()
+        ]);
+    }
 
-        //recuperer le country a patir du slug
-        //faire une recherhe sur les articles where country = a lobjet country que tu as recuepere depuis la base de donnees
-        $articles = $this->articleRepository->findPaginatedArticles([$from]);
-        //$articles = $this->articleRepository->getArticlesByCountry(["country" => $country]);
-
-
-        return $this->render('user/pages/country.html.twig', ["articles"=> $articles]);
-    } */
 }
