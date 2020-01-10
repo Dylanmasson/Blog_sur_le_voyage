37f7407f39ece6920e6651381bf45bbdcea74b69
valerie-geist <geistval@gmail.com>
2020-01-10 16:08:51 +0100

    Création page continent.html.twig Update ContinentController.php et HomeController.php update country.html.twig et home.html.twig et general_style.css Création UserController.php et user.html.twig Modification routes

--- src/Controller/ContinentController.php
+++ src/Controller/ContinentController.php
@@ -2,16 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ContinentRepository;
+use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class ContinentController extends AbstractController
 {
-    /**
-     * @Route("/continent", name="continent")
-     */
-    public function index()
+
+    private $continentRepository;
+    private $countryRepository;
+
+    public function __construct(ContinentRepository $continentRepository, CountryRepository $countryRepository)
     {
-        return $this->render('');
+        $this->continentRepository = $continentRepository;
+        $this->countryRepository = $countryRepository;
     }
+
+// Rajout du slug plus tard
+    public function continentAction($name){
+        $continent = $this->continentRepository->findOneBy(["name" => $name]);
+        $countries = $this->countryRepository->findBy(["continent" => $continent]);
+
+        return $this->render('user/pages/continent.html.twig', ["countries" => $countries, "name" => $name]);
+    }
+
+
+
 }
